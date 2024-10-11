9a08fa97bb792c42aac85f02b94540151551cce0
thachit <nguyencothach1989@gmail.com>
2024-10-12 06:19:04 +0700

    Add models and database migration

--- src/core/config.py
+++ src/core/config.py
@@ -4,5 +4,10 @@ from pydantic import BaseSettings
 class Config:
     APP_NAME = os.getenv("APP_NAME", "My Fast API app")
     ADMIN_EMAIL = os.getenv("ADMIN_EMAIL", "nguyencothach1989@gmail.com")
-    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
+    DATABASE_DRIVER = os.getenv("DATABASE_DRIVER", "postgresql+psycopg2")
+    DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
+    DATABASE_PORT = os.getenv("DATABASE_PORT", "5432")
+    DATABASE_USER = os.getenv("DATABASE_USER", "postgres")
+    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", None)
+    DATABASE_NAME = os.getenv("DATABASE_NAME", "postgres")
     DEBUG = os.getenv("DEBUG", "false").lower() in ("true", 1)
